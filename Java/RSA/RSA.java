87a54b7c942c2084286660eb59af6688b3e307e4
NamrathaVenkatesh <namrathavenkateshmurthy@gmail.com>
2018-07-08 23:49:40 -0400

    moduluspower

--- Java/RSA/RSA.java
+++ Java/RSA/RSA.java
@@ -1,6 +1,6 @@
 import java.math.*;
 public class RSA{
-	
+
 	public boolean isPrime(int num){
 		if(num == 0 || num ==1){
 			return false;
@@ -12,7 +12,7 @@ public class RSA{
 		}
 		return true;
 	}
-	
+
 	public int primeNumber(int min,int max){
 		if(min==0 || max==0 || max<min)
 			return 0;
@@ -25,37 +25,47 @@ public class RSA{
 		}
 		return 0;
 	}
-	
+
 	public double modulusPower(int base,int exp, int mod){
-		
-		return 0;
+
+	    if (base < 1 || exp < 0 || mod < 1)
+        return 0;
+
+    int result = 1;
+    while (exp > 0) {
+        result = (result * base) % mod;
+       exp--;
+    }
+    return result;
+
+
 	}
-	
+
 	public int lcm(int a, int b){
-		
+
 		return 0;
 	}
-	
+
 	public int coprime(int a){
-		
+
 		return 0;
 	}
-	
+
 	public String encrypt(String msg,int n, int publicKey){
 		String encryptedMsg = "";
-		
+
 		return encryptedMsg;
 	}
-	
+
 	public String decrypt(String encryptedMsg,int n, int privateKey){
 		String decryptedMsg = "";
-		
+
 		return decryptedMsg;
 	}
-	
+
 	public String chineseRemainderTheorem(String encryptedMsg,int p, int q, int privateKey){
 		String decryptedMsg = "";
-		
+
 		return decryptedMsg;
 	}
 }
